a6c1115552bb784fc0eef717cd574d8bcbcd6a4c
samyunwei <259375702@qq.com>
2020-03-22 21:52:16 +0800

    添加了掌纹识别类

---
+++ PalmRecognition.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+from numpy.fft import fft2, ifft2, fftshift
+import pprint
+import os
+from enum import Enum
+import math as mth
+from scipy import signal
+
+class PalmRecognition(object):
+    pass
